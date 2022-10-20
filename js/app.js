8f2961aaa9081e1434ad262071529f1ae6bcb271
Israel Stoba <51468181+Israelstoba@users.noreply.github.com>
2022-10-20 13:04:12 +0100

    new mavic update for product page and cart

--- js/app.js
+++ js/app.js
@@ -1,47 +1,52 @@
 ////////js for the navigation menu
-var menuOpen = document.getElementById("menu-toggler");
-var menuClose = document.getElementById("menu-toggler");
+var menuOpen = document.getElementById('menu-toggler');
+var menuClose = document.getElementById('menu-toggler');
 
-menuOpen.style.maxHeight = "0px";
-menuClose.style.maxHeight = "0px";
+menuOpen.style.maxHeight = '0px';
+menuClose.style.maxHeight = '0px';
 
 function menutoggle() {
-  if (menuOpen.style.maxHeight == "0px") {
-    menuOpen.style.maxHeight = "550px";
+  if (menuOpen.style.maxHeight == '0px') {
+    menuOpen.style.maxHeight = '550px';
   } else {
-    menuOpen.style.maxHeight = "0px";
+    menuOpen.style.maxHeight = '0px';
   }
 }
 
 function menutoggle() {
-  if (menuClose.style.maxHeight == "550px") {
-    menuClose.style.maxHeight = "0px";
+  if (menuClose.style.maxHeight == '550px') {
+    menuClose.style.maxHeight = '0px';
   } else {
-    menuOpen.style.maxHeight = "550px";
+    menuOpen.style.maxHeight = '550px';
   }
 }
 
-////////Quantity increament and decreament
+/////////////// CART /////////////////////////
 
-var quantity = document.getElementById("quantity");
-
-var increment = 0;
-var decrement = 0;
-
-function qtyIncrement() {
-  decrement = 1;
-  increment += +1;
-  quantity.value = increment + 1;
+////////////removing products from the cart
+var removeCartItemButton = document.getElementsByClassName('remove-btn');
+console.log(removeCartItemButton);
+for (var i = 0; i < removeCartItemButton.length; i++) {
+  let button = removeCartItemButton[i];
+  button.addEventListener('click', function (event) {
+    var buttonClicked = event.target;
+    buttonClicked.parentElement.parentElement.remove();
+    updateCartTotal();
+  });
 }
 
-function qtyDecrement() {
-  increment += -1;
-  decrement = increment + 1;
-  quantity.value = decrement;
-  /////to avoid nagatives quantity values
-  if (quantity.value <= 0) {
-    quantity.value = 1;
-    increment = 0;
-    decrement = 0;
+//////////////updating cart total
+function updateCartTotal() {
+  var cartItemContainer = document.getElementsByClassName(
+    'cart-items-container'
+  )[0];
+  var cartRows = cartItemContainer.document.getElementsByClassName('cart-row');
+  for (var i = 0; i < cartRows.length; i++) {
+    var cartRow = cartRows[i];
+    var priceElement = cartRow.getElementsByClassName('cart-price')[0];
+    var quantityElement = cartRow.getElementsByClassName(
+      'cart-quantity-input'
+    )[0];
+    var price = priceElement.innerText;
   }
 }
