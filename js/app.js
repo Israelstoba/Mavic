7f4547c77b992c3701331e2a00827b06044e529b
israelstoba <israelstoba@gmail.com>
2023-04-24 05:59:52 +0100

    whatsapp ordering integrated

--- js/app.js
+++ js/app.js
@@ -21,32 +21,67 @@ function menutoggle() {
   }
 }
 
-/////////////// CART /////////////////////////
+// /////////////// CART /////////////////////////
+// var removeCartItemButton = document.getElementsByClassName('remove-btn');
 
-////////////removing products from the cart
-var removeCartItemButton = document.getElementsByClassName('remove-btn');
-console.log(removeCartItemButton);
-for (var i = 0; i < removeCartItemButton.length; i++) {
-  let button = removeCartItemButton[i];
-  button.addEventListener('click', function (event) {
-    var buttonClicked = event.target;
-    buttonClicked.parentElement.parentElement.remove();
-    updateCartTotal();
+// for (var i = 0; i < removeCartItemButton.length; i++) {
+//   var button = removeCartItemButton[i];
+//   button.addEventListener('click', function (event) {
+//     var buttonClicked = event.target;
+//     buttonClicked.parentElement.parentElement.remove();
+//     updateCartTotal();
+//   });
+// }
+
+// function updateCartTotal() {
+//   var cartItemContainer = document.getElementsByClassName(
+//     'cart-items-container'
+//   )[0];
+//   var cartRows = cartItemContainer.document.getElementsByClassName('cart-row');
+//   for (var i = 0; i < cartRows.length; i++) {
+//     var cartRow = cartRows[i];
+//     var priceElement = cartRow.getElementsByClassName('cart-price')[0];
+//     var quantityElement = cartRow.getElementsByClassName(
+//       'cart-quantity-input'
+//     )[0];
+
+//     var price = parseFloat(priceElement.innerText.replace('$', ''));
+//     var quantity = quantityElement.value;
+//     console.log(price * quantity);
+//   }
+// }
+
+///////////////// WHATSAPP SCRIPT ////////////////
+var linkBtns = document.querySelectorAll('.link');
+
+for (var i = 0; i < linkBtns.length; i++) {
+  linkBtns[i].addEventListener('click', function () {
+    var formCon = document.querySelector('.customer-info-form-con');
+    formCon.classList.add('show');
+
+    const closeBtn = document.querySelector('.close-btn');
+    closeBtn.addEventListener('click', () => {
+      formCon.classList.remove('show');
+    });
+
+    const overlay = document.querySelector('.overlay');
+    overlay.addEventListener('click', function () {
+      formCon.classList.remove('show');
+    });
+
+    const proceedBtn = document.querySelector('#proceed-btn');
+    const customerName = document.querySelector('#customer-name');
+    const customerNumber = document.querySelector('#customer-number');
+    const customerAddress = document.querySelector('#customer-address');
+    var productName =
+      this.parentElement.querySelector('.product-name').textContent;
+    const message = document.querySelector('#message');
+    message.value = productName;
+
+    proceedBtn.addEventListener('click', () => {
+      var waUrl = `https://wa.me/+2348056101383?text= Product Name: ${productName} %0a Name: ${customerName.value} %0a Phone: ${customerNumber.value} %0a Address: ${customerAddress.value}. %0a I will like to place an order for the above item`;
+      window.open(waUrl, '_blank', focus()); // this opens a new browser tab
+      // window.open(waUrl, '_blank', 'focus'); // this opens a new browser window
+    });
   });
-}
-
-//////////////updating cart total
-function updateCartTotal() {
-  var cartItemContainer = document.getElementsByClassName(
-    'cart-items-container'
-  )[0];
-  var cartRows = cartItemContainer.document.getElementsByClassName('cart-row');
-  for (var i = 0; i < cartRows.length; i++) {
-    var cartRow = cartRows[i];
-    var priceElement = cartRow.getElementsByClassName('cart-price')[0];
-    var quantityElement = cartRow.getElementsByClassName(
-      'cart-quantity-input'
-    )[0];
-    var price = priceElement.innerText;
-  }
 }
